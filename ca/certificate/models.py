153ec56844a8c026a810c2876a05be86256e5e0c
Mathias Ertl <mati@fsinf.at>
2013-08-09 20:29:34 +0200

    add bare certificate model

--- ca/certificate/models.py
+++ ca/certificate/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Certificate(models):
+    created = models.DateTimeField(auto_now=True)
+    expires = models.DateTimeField(null=False, blank=False)
+
+    csr = models.TextField(null=False, blank=False)
+    pub = models.TextField(null=False, blank=False)
+
+    cn = models.CharField(max_length=64, null=False, blank=False)
