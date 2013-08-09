3906605f2b61d03bdbdbc678307726cf939c21de
Mathias Ertl <mati@fsinf.at>
2013-08-09 21:31:25 +0200

    add basic settings

--- ca/ca/settings.py
+++ ca/ca/settings.py
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'ca.sqlite3',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -124,6 +124,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+
+    'certificate',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -154,3 +156,7 @@ LOGGING = {
         },
     }
 }
+
+CA_PRIVATE_KEY = '/home/mati/git/fsinf/certificate-authority/priv/priv.key'
+CA_PUBLIC_KEY = '/home/mati/git/fsinf/certificate-authority/fsinf_at.crt'
+DIGEST_ALGORITHM = 'md5'
