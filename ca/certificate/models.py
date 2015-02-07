a734a205204e10696563c895fbf36ab557b9a1a8
Mathias Ertl <mati@er.tl>
2015-02-07 14:46:54 +0100

    add method to get an x509

--- ca/certificate/models.py
+++ ca/certificate/models.py
@@ -1,10 +1,14 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from OpenSSL import crypto
+
 from certificate.managers import CertificateManager
 
 
 class Certificate(models.Model):
+    _x509 = None
+
     objects = CertificateManager()
 
     watchers = models.ManyToManyField(User)
@@ -16,3 +20,9 @@ class Certificate(models.Model):
     pub = models.TextField(null=False, blank=False)
 
     cn = models.CharField(max_length=64, null=False, blank=False)
+
+    @property
+    def x509(self):
+        if self._x509 is None:
+            self._x509 = crypto.load_certificate(crypto.FILETYPE_PEM, self.pub)
+        return self._x509
