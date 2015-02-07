14dcbae2c2a26cd204cda3fe51126762070fb759
Mathias Ertl <mati@er.tl>
2015-02-07 14:47:56 +0100

    use x509 prop

--- ca/certificate/management/commands/view.py
+++ ca/certificate/management/commands/view.py
@@ -2,10 +2,7 @@ import sys
 
 from datetime import datetime
 
-from OpenSSL import crypto
-
 from django.core.management.base import BaseCommand
-from django.utils import six
 
 from certificate.models import Certificate
 
@@ -27,12 +24,11 @@ class Command(BaseCommand):
         except Certificate.DoesNotExist:
             self.stderr.write('Certificate with given ID not found.')
             sys.exit(1)
-        x509 = crypto.load_certificate(crypto.FILETYPE_PEM, cert.pub)
 
         print('Common Name: %s' % cert.cn)
 
-        for i in range(0, x509.get_extension_count()):
-            ext = x509.get_extension(i)
+        for i in range(0, cert.x509.get_extension_count()):
+            ext = cert.x509.get_extension(i)
             if ext.get_short_name() == 'subjectAltName':
                 names = ext.get_data().lstrip('0D\x82\x0f').split('\x82\x0f')
                 print('Alternative Names: %s' % ', '.join(names))
@@ -42,14 +38,14 @@ class Command(BaseCommand):
         emails = [w.email for w in cert.watchers.all()]
         print('Watchers: %s' % ', '.join(emails))
 
-        validFrom = datetime.strptime(x509.get_notBefore(), DATE_FMT)
-        validUntil = datetime.strptime(x509.get_notAfter(), DATE_FMT)
+        validFrom = datetime.strptime(cert.x509.get_notBefore(), DATE_FMT)
+        validUntil = datetime.strptime(cert.x509.get_notAfter(), DATE_FMT)
 
         print('Valid from: %s' % validFrom.strftime('%Y-%m-%d %H:%M'))
         print('Valid until: %s' % validUntil.strftime('%Y-%m-%d %H:%M'))
 
         print('Digest:')
-        print('    md5: %s' % x509.digest('md5'))
-        print('    sha1: %s' % x509.digest('sha1'))
-        print('    sha256: %s' % x509.digest('sha256'))
-        print('    sha512: %s' % x509.digest('sha512'))
+        print('    md5: %s' % cert.x509.digest('md5'))
+        print('    sha1: %s' % cert.x509.digest('sha1'))
+        print('    sha256: %s' % cert.x509.digest('sha256'))
+        print('    sha512: %s' % cert.x509.digest('sha512'))
