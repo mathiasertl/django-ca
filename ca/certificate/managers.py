46bb09c482354ca3783a7ce0eb7147e2942458ab
Mathias Ertl <mati@er.tl>
2015-02-07 19:36:27 +0100

    use correct x509 for subjectKeyIdentifier in client certs

--- ca/certificate/managers.py
+++ ca/certificate/managers.py
@@ -101,7 +101,7 @@ class CertificateManager(models.Manager):
         extensions.append(crypto.X509Extension(str('extendedKeyUsage'), 0,
                                                str(','.join(ext_key_usage))))
         extensions.append(crypto.X509Extension(str('subjectKeyIdentifier'), 0, str('hash'),
-                                               subject=issuerPub))
+                                               subject=cert))
         extensions.append(crypto.X509Extension(str('authorityKeyIdentifier'), 0,
                                                str('keyid,issuer'), issuer=issuerPub))
 
