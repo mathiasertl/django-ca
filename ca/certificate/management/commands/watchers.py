d1edded4529724547938af322862ec0f7b078d4c
Mathias Ertl <mati@er.tl>
2015-02-08 09:52:16 +0100

    cleanup the watchers command

--- ca/certificate/management/commands/watchers.py
+++ ca/certificate/management/commands/watchers.py
@@ -1,16 +1,32 @@
-import sys
+# -*- coding: utf-8 -*-
+#
+# This file is part of fsinf-certificate-authority
+# (https://github.com/fsinf/certificate-authority).
+#
+# fsinf-certificate-authority is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# fsinf-certificate-authority is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# fsinf-certificate-authority.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
 
 from optparse import make_option
 
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
-from django.utils import six
+from django.core.management.base import CommandError
 
 from certificate.models import Certificate
 
 
 class Command(BaseCommand):
-    args = '<id>'
+    args = '<serial>'
     help = "Add/remove watchers to a specific certificate."
 
     option_list = BaseCommand.option_list + (
@@ -32,14 +48,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if len(args) != 1:
-            self.stderr.write(
-                "Please give exactly one ID (first colum of list command)")
-            sys.exit()
+            raise CommandError("Please give exactly one serial (first colum of list command).")
+
         try:
-            cert = Certificate.objects.get(pk=args[0])
+            cert = Certificate.objects.get(serial=args[0])
         except Certificate.DoesNotExist:
-            self.stderr.write('Certificate with given ID not found.')
-            sys.exit(1)
+            raise CommandError('Certificate with given serial not found.')
 
         # add users:
         add = [User.objects.get_or_create(email=e, defaults={'username': e})[0]
