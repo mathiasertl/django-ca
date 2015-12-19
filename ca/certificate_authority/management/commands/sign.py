f4b23aa7b9a8d0499ff6b90f76197694b9f0213b
Mathias Ertl <mati@er.tl>
2015-12-19 20:09:44 +0100

    switch to argparse

--- ca/certificate_authority/management/commands/sign.py
+++ ca/certificate_authority/management/commands/sign.py
@@ -1,5 +1,3 @@
-from optparse import make_option
-
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
@@ -11,47 +9,34 @@ from certificate_authority.models import Certificate
 class Command(BaseCommand):
     help = "Sign a CSR and output signed certificate."
 
-    option_list = BaseCommand.option_list + (
-        make_option('--days',
-            default=720,
-            type='int',
-            help='Sign the certificate for DAYS days (default: %default)'
-        ),
-        make_option(
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--days', default=720, type=int,
+            help='Sign the certificate for DAYS days (default: %(default)s)')
+        parser.add_argument(
             '--algorithm',
-            help='Algorithm to use (default: The DIGEST_ALGORITHM setting in settings.py)'
-        ),
-        make_option(
-            '--csr',
-            metavar='FILE',
-            help='The path to the certificate to sign, if ommitted, you will be be prompted.'
-        ),
-        make_option(
-            '--alt',
-            metavar='DOMAIN',
-            action='append',
-            default=[],
-            help='Add a subjectAltName to the certificate (may be given multiple times)'
-        ),
-        make_option(
-            '--watch',
-            metavar='EMAIL',
-            action='append',
-            default=[],
-            help='Email EMAIL when this certificate expires (may be given multiple times)',
-        ),
-        make_option(
-            '--out',
-            metavar='FILE',
-            help='Save signed certificate to FILE. If omitted, print to stdout.'
-        ),
-        make_option('--key-usage', default=','.join(settings.CA_KEY_USAGE), metavar='NAMES',
-                    help="Override keyUsage attribute (default: CA_KEY_USAGE setting: %default)."),
-        make_option('--ext-key-usage', default=','.join(settings.CA_EXT_KEY_USAGE), metavar='NAMES',
-                    help="Override keyUsage attribute (default: CA_EXT_KEY_USAGE setting: %default)."),
-        make_option('--ocsp', default=False, action='store_true',
-                    help="Issue a certificate for an OCSP server.")
-    )
+            help='Algorithm to use (default: The DIGEST_ALGORITHM setting in settings.py)')
+        parser.add_argument(
+            '--csr', metavar='FILE',
+            help='The path to the certificate to sign, if ommitted, you will be be prompted.')
+        parser.add_argument(
+            '--alt', metavar='DOMAIN', action='append', default=[],
+            help='Add a subjectAltName to the certificate (may be given multiple times)')
+        parser.add_argument(
+            '--watch', metavar='EMAIL', action='append', default=[],
+            help='Email EMAIL when this certificate expires (may be given multiple times)')
+        parser.add_argument(
+            '--out', metavar='FILE',
+            help='Save signed certificate to FILE. If omitted, print to stdout.')
+        parser.add_argument(
+            '--key-usage', default=','.join(settings.CA_KEY_USAGE), metavar='NAMES',
+            help="Override keyUsage attribute (default: CA_KEY_USAGE setting: %(default)s).")
+        parser.add_argument(
+            '--ext-key-usage', default=','.join(settings.CA_EXT_KEY_USAGE), metavar='NAMES',
+            help="Override keyUsage attribute (default: CA_EXT_KEY_USAGE setting: %(default)s).")
+        parser.add_argument(
+            '--ocsp', default=False, action='store_true',
+            help="Issue a certificate for an OCSP server.")
 
     def handle(self, *args, **options):
         if options['csr'] is None:
