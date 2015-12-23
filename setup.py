f4188ddbdb49279572f4c544ba33041f1ab03ae8
Mathias Ertl <mati@er.tl>
2015-12-23 16:23:25 +0100

    add first setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-ca',
+      version='1.0.0',
+      description='',
+      author='Mathias Ertl',
+      author_email='mati@er.tl',
+      url='https://github.com/mathiasertl/django-ca',
+      packages=['django_ca'],
+     )
