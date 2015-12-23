416df2ef6fdfef303fae8a62231289c928742b21
Mathias Ertl <mati@er.tl>
2015-12-23 14:33:30 +0100

    remove the sites framework

--- ca/ca/settings.py
+++ ca/ca/settings.py
@@ -107,7 +107,6 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
