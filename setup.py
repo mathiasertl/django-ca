d46015cd2801cbbf9cf6591b89d28778d0187d2f
Mathias Ertl <mati@er.tl>
2015-12-23 16:48:14 +0100

    add manifest, finish setup

--- setup.py
+++ setup.py
@@ -1,12 +1,40 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+import os
 
 from distutils.core import setup
 
-setup(name='django-ca',
-      version='1.0.0',
-      description='',
-      author='Mathias Ertl',
-      author_email='mati@er.tl',
-      url='https://github.com/mathiasertl/django-ca',
-      packages=['django_ca'],
-     )
+my_path = os.path.normpath(os.path.abspath(__file__))
+my_base = os.path.dirname(my_path)
+
+dist_dir = os.path.join(my_base, 'dist')
+template = os.path.join(my_base, 'MANIFEST.in')
+
+os.chdir(os.path.join(my_base, 'ca'))
+
+setup(
+    name='django-ca',
+    version='1.0.0',
+    description='',
+    author='Mathias Ertl',
+    author_email='mati@er.tl',
+    url='https://github.com/mathiasertl/django-ca',
+    packages=[
+        'django_ca',
+        'django_ca.management',
+        'django_ca.management.commands',
+        'django_ca.migrations',
+    ],
+    classifiers=[
+        'Framework :: Django',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    options={
+        'sdist': {
+            'dist_dir': dist_dir,
+            'template': template,
+        },
+    }
+)
