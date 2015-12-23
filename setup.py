057d1940bb784f8491b41d6fc1f9d2ba6429da9e
Mathias Ertl <mati@er.tl>
2015-12-23 17:30:25 +0100

    use package_dir options

--- setup.py
+++ setup.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python3
 
-import os
-
 from distutils.core import setup
-
-my_path = os.path.normpath(os.path.abspath(__file__))
-my_base = os.path.dirname(my_path)
-
-dist_dir = os.path.join(my_base, 'dist')
-template = os.path.join(my_base, 'MANIFEST.in')
-
-os.chdir(os.path.join(my_base, 'ca'))
 
 setup(
     name='django-ca',
@@ -25,6 +15,7 @@ setup(
         'django_ca.management.commands',
         'django_ca.migrations',
     ],
+    package_dir={'': 'ca'},
     install_requires=[
         'Django==1.9',
         'pyOpenSSL==0.15.1',
@@ -35,10 +26,4 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
-    options={
-        'sdist': {
-            'dist_dir': dist_dir,
-            'template': template,
-        },
-    }
 )
