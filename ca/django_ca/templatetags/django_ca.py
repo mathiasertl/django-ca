b28a705e003e9c8044b0975da066408f70165429
Mathias Ertl <mati@er.tl>
2015-12-28 16:47:31 +0100

    add custom template tags

---
+++ ca/django_ca/templatetags/django_ca.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of django-ca (https://github.com/mathiasertl/django-ca).
+#
+# django-ca is free software: you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# django-ca is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with django-ca.  If not,
+# see <http://www.gnu.org/licenses/>.
+
+from django import template
+from django.contrib.admin.templatetags.admin_modify import submit_row
+
+register = template.Library()
+
+@register.inclusion_tag('admin/submit_line.html', takes_context=True)
+def django_ca_certificate_submit_row(context):
+    ctx = submit_row(context)
+    ctx['show_delete_link'] = False
+    ctx['show_save_and_add_another'] = False
+    return ctx
