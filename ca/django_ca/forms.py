ee95efdce09dff4ad5ebaf6b3678aade771c81c2
Mathias Ertl <mati@er.tl>
2015-12-31 16:37:43 +0100

    add an expires field, use MultipleChoiceFields for keyUsage and extendedKeyUsage

--- ca/django_ca/forms.py
+++ ca/django_ca/forms.py
@@ -13,17 +13,46 @@
 # You should have received a copy of the GNU General Public License along with django-ca.  If not,
 # see <http://www.gnu.org/licenses/>.
 
+from datetime import datetime
+from datetime import timedelta
+
 from django import forms
+from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
 from .fields import BasicConstraintsField
 from .models import Certificate
 
 
+def _initial_expires():
+    return datetime.today() + timedelta(days=settings.CA_DEFAULT_EXPIRES)
+
+
 class CreateCertificateForm(forms.ModelForm):
+    expires = forms.DateField(initial=_initial_expires)
     subjectAltName = forms.CharField(label='subjectAltName', required=False)
-    keyUsage = forms.CharField(label='keyUsage', required=False)
-    extendedKeyUsage = forms.CharField(label='extendedKeyUsage', required=False)
+    keyUsage = forms.MultipleChoiceField(label='keyUsage', required=False, choices=(
+        ('cRLSign', 'cRLSign'),
+        ('dataEncipherment', 'dataEncipherment'),
+        ('decipherOnly', 'decipherOnly'),
+        ('digitalSignature', 'digitalSignature'),
+        ('encipherOnly', 'encipherOnly'),
+        ('keyAgreement', 'keyAgreement'),
+        ('keyCertSign', 'keyCertSign'),
+        ('keyEncipherment', 'keyEncipherment'),
+        ('nonRepudiation', 'nonRepudiation'),
+    ))
+    extendedKeyUsage = forms.MultipleChoiceField(label='extendedKeyUsage', required=False, choices=(
+        ('serverAuth', 'SSL/TLS Web Server Authentication'),
+        ('clientAuth', 'SSL/TLS Web Client Authentication'),
+        ('codeSigning', 'Code signing'),
+        ('emailProtection', 'E-mail Protection (S/MIME)'),
+        ('timeStamping', 'Trusted Timestamping'),
+        ('msCodeInd', 'Microsoft Individual Code Signing (authenticode)'),
+        ('msCodeCom', 'Microsoft Commercial Code Signing (authenticode)'),
+        ('msCTLSign', 'Microsoft Trust List Signing'),
+        ('msEFS', 'Microsoft Encrypted File System'),
+    ))
     basicConstraints = BasicConstraintsField(label='basicConstraints')
 
     class Meta:
