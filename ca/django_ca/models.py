66c399a06c1cbbb486992bcf6711e5e0905bc2dd
Mathias Ertl <mati@er.tl>
2016-01-03 14:22:50 +0100

    fix automatic setting of cn

--- ca/django_ca/models.py
+++ ca/django_ca/models.py
@@ -103,7 +103,7 @@ class Certificate(models.Model):
     authorityKeyIdentifier.short_description = 'authorityKeyIdentifier'
 
     def save(self, *args, **kwargs):
-        if self.pk is None and self.cn is None:
+        if self.pk is None and not self.cn:
             self.cn = dict(self.x509.get_subject().get_components()).get(b'CN').decode('utf-8')
         if self.pk is None or self.serial is None:
             self.serial = hex(self.x509.get_serial_number())[2:].upper()
