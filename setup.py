2f3ea36bb8c214abb5d3e44f48906667b2cdbf58
Mathias Ertl <mati@er.tl>
2016-03-05 14:10:27 +0100

    also exclude tests

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ class CoverageCommand(BaseCommand):
 
         cov = coverage.Coverage(cover_pylib=False, branch=True,
                                 source=['django_ca'],
-                                omit=['*migrations/*.py', ]
+                                omit=['*migrations/*', '*/tests.py' ]
                                )
         cov.start()
 
