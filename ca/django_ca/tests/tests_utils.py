ba81499e17fc95b5c08637819ef87853f7b7ae7f
Mathias Ertl <mati@er.tl>
2016-03-05 16:36:42 +0100

    add some more tests

--- ca/django_ca/tests/tests_utils.py
+++ ca/django_ca/tests/tests_utils.py
@@ -5,6 +5,8 @@ from datetime import datetime
 from django.test import TestCase
 
 from django_ca.utils import format_date
+from django_ca.utils import get_basic_cert
+from django_ca.utils import is_power2
 from django_ca.utils import parse_date
 
 
@@ -22,3 +24,24 @@ class FormatDateTestCase(TestCase):
         d = datetime(2016, 3, 5, 14, 53, 12)
         f = format_date(d)
         self.assertEqual(d, parse_date(f))
+
+
+class Power2TestCase(TestCase):
+    def test_true(self):
+        for i in range(0, 20):
+            self.assertTrue(is_power2(2 ** i))
+
+    def test_false(self):
+        self.assertFalse(is_power2(0))
+        self.assertFalse(is_power2(3))
+        self.assertFalse(is_power2(5))
+
+        for i in range(2, 20):
+            self.assertFalse(is_power2((2 ** i) - 1))
+            self.assertFalse(is_power2((2 ** i) + 1))
+
+
+class GetBasicCertTestCase(TestCase):
+    def test_basic(self):
+        cert = get_basic_cert(720)
+        self.assertFalse(cert.has_expired())
