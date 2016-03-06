b99fbc81aeaff63c9060146bbed4ec6aaa09de8e
Mathias Ertl <mati@er.tl>
2016-03-06 13:29:29 +0100

    use qs init() method for init_ca, fix override_settings for class decoration

--- ca/django_ca/tests/base.py
+++ ca/django_ca/tests/base.py
@@ -10,16 +10,22 @@ import shutil
 import subprocess
 import tempfile
 
-from django.core.management import call_command
 from django.test import TestCase
 from django.test.utils import override_settings as _override_settings
 from django.utils.six.moves import reload_module
 
 from django_ca import ca_settings
+from django_ca.models import CertificateAuthority
 
 
 class override_settings(_override_settings):
     """Enhance override_settings to also reload django_ca.ca_settings."""
+
+    def __call__(self, test_func):
+        if isinstance(test_func, type) and not issubclass(test_func, DjangoCATestCase):
+            raise Exception("Only subclasses of DjangoCATestCase can use override_settings.")
+        inner = super(override_settings, self).__call__(test_func)
+        return inner
 
     def save_options(self, test_func):
         super(override_settings, self).save_options(test_func)
@@ -52,6 +58,13 @@ class override_tmpcadir(override_settings):
 class DjangoCATestCase(TestCase):
     """Base class for all testcases with some enhancements."""
 
+    @classmethod
+    def setUpClass(cls):
+        super(DjangoCATestCase, cls).setUpClass()
+
+        if cls._overridden_settings:
+            reload_module(ca_settings)
+
     def setUp(self):
         reload_module(ca_settings)
 
@@ -64,8 +77,9 @@ class DjangoCATestCase(TestCase):
     @classmethod
     def init_ca(cls, **kwargs):
         kwargs.setdefault('key_size', 2048)
-        call_command('init_ca', 'Root CA', 'AT', 'Vienna', 'Vienna', 'HTU', 'FSINF', 'ca.fsinf.at',
-                     **kwargs)
+        return CertificateAuthority.objects.init(
+            name='Root CA', key_type='RSA', algorithm='sha256', expires=720, parent=None, pathlen=0,
+            subject={'CN': 'ca.example.com', }, **kwargs)
 
     def create_csr(self, name='example.com', key_size=512):
         key = os.path.join(ca_settings.CA_DIR, '%s.key' % name)
