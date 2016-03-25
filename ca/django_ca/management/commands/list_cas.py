f2df33245006b7b7d4d110349df8d7da9ee08fce
Mathias Ertl <mati@er.tl>
2016-03-25 23:26:08 +0100

    add list_cas command

---
+++ ca/django_ca/management/commands/list_cas.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of django-ca (https://github.com/mathiasertl/django-ca).
+#
+# django-ca is free software: you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# django-ca is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with django-ca.  If not,
+# see <http://www.gnu.org/licenses/>.
+
+from ..base import BaseCommand
+from ...models import CertificateAuthority
+
+
+class Command(BaseCommand):
+    help = 'List available certificate authorities.'
+
+    def handle(self, **options):
+        for ca in CertificateAuthority.objects.all():
+            self.stdout.write(
+                '%s: %s%s' % (ca.serial, ca.name, ' (disabled)' if not ca.enabled else ''))
