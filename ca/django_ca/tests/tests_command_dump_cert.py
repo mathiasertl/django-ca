850e5e97938019761afaa3b68e09b535ca4089df
Mathias Ertl <mati@er.tl>
2016-04-16 12:15:43 +0200

    add tests for the dump_cert command

---
+++ ca/django_ca/tests/tests_command_dump_cert.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of django-ca (https://github.com/mathiasertl/django-ca).
+#
+# django-ca is free software: you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# django-ca is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with django-ca.  If not,
+# see <http://www.gnu.org/licenses/>
+
+import os
+
+from io import BytesIO
+
+from OpenSSL import crypto
+
+from django.core.management.base import CommandError
+
+from .. import ca_settings
+from .base import DjangoCAWithCertTestCase
+from .base import override_tmpcadir
+
+
+@override_tmpcadir(CA_MIN_KEY_SIZE=1024, CA_PROFILES={}, CA_DEFAULT_SUBJECT={})
+class DumpCertTestCase(DjangoCAWithCertTestCase):
+    def test_basic(self):
+        stdout, stderr = self.cmd('dump_cert', self.cert.serial, 
+                                  stdout=BytesIO(), stderr=BytesIO())
+        self.assertEqual(stderr, b'')
+        self.assertEqual(stdout, self.cert.pub.encode('utf-8'))
+
+    def test_format(self):
+        for option in ['PEM', 'TEXT', 'ASN1']:
+            format = getattr(crypto, 'FILETYPE_%s' % option)
+            stdout, stderr = self.cmd('dump_cert', self.cert.serial, format=format,
+                                      stdout=BytesIO(), stderr=BytesIO())
+            self.assertEqual(stderr, b'')
+            self.assertEqual(stdout, crypto.dump_certificate(format, self.cert.x509))
+
+    def test_explicit_stdout(self):
+        stdout, stderr = self.cmd('dump_cert', self.cert.serial, '-',
+                                  stdout=BytesIO(), stderr=BytesIO())
+        self.assertEqual(stderr, b'')
+        self.assertEqual(stdout, self.cert.pub.encode('utf-8'))
+
+    def test_file_output(self):
+        path = os.path.join(ca_settings.CA_DIR, 'test_cert.pem')
+        stdout, stderr = self.cmd('dump_cert', self.cert.serial, path,
+                                  stdout=BytesIO(), stderr=BytesIO())
+        self.assertEqual(stderr, b'')
+        self.assertEqual(stdout, b'')
+
+        with open(path) as stream:
+            self.assertEqual(stream.read(), self.cert.pub)
+
+    def test_wrong_path(self):
+        path = os.path.join(ca_settings.CA_DIR, 'does-not-exist', 'test_cert.pem')
+        with self.assertRaises(CommandError):
+            self.cmd('dump_cert', self.cert.serial, path, stdout=BytesIO(),
+                     stderr=BytesIO())
