7bf24105854733b8db544bc190c419d4ac7ef70f
Mathias Ertl <mati@er.tl>
2016-05-10 23:22:38 +0200

    start writing a OCSP test class

---
+++ ca/django_ca/tests/tests_views_ocsp.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of django-ca (https://github.com/mathiasertl/django-ca).
+#
+# django-ca is free software: you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# django-ca is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with django-ca.  If not,
+# see <http://www.gnu.org/licenses/>
+
+import os
+
+from django.core.management.base import CommandError
+from django.utils import six
+
+from .. import ca_settings
+from ..models import Certificate
+from .base import DjangoCAWithCSRTestCase
+from .base import override_settings
+from .base import override_tmpcadir
+
+
+class OCSPTestView(object):
+    def get_request(self):
+        #openssl ocsp -CAfile files/ca.pem -issuer files/ca.pem -serial 123  -reqout file -resp_text
+        pass
