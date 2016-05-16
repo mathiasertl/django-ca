f1fbe78a3a46a99f00d36e8b846aff3e7757f549
Mathias Ertl <mati@er.tl>
2016-05-16 12:02:39 +0200

    add first crude test

--- ca/django_ca/tests/tests_views_ocsp.py
+++ ca/django_ca/tests/tests_views_ocsp.py
@@ -13,19 +13,40 @@
 # You should have received a copy of the GNU General Public License along with django-ca.  If not,
 # see <http://www.gnu.org/licenses/>
 
+import base64
 import os
 
-from django.core.management.base import CommandError
-from django.utils import six
+from django.conf.urls import url
+from django.core.urlresolvers import reverse
+from django.test import Client
 
-from .. import ca_settings
-from ..models import Certificate
-from .base import DjangoCAWithCSRTestCase
+from ..views import OCSPView
+from .base import DjangoCAWithCertTestCase
+from .base import fixtures_dir
 from .base import override_settings
-from .base import override_tmpcadir
 
 
-class OCSPTestView(object):
-    def get_request(self):
-        #openssl ocsp -CAfile files/ca.pem -issuer files/ca.pem -serial 123  -reqout file -resp_text
-        pass
+#openssl ocsp -CAfile files/ca.pem -issuer files/ca.pem -serial 123  -reqout file -resp_text
+def _load_req(req):
+    path = os.path.join(fixtures_dir, 'ocsp', req)
+    with open(path, 'rb') as stream:
+        return stream.read()
+
+req1 = _load_req('req1')
+
+urlpatterns = [
+    url(r'^ocsp/(?P<data>[a-zA-Z0-9=+/]+)$', OCSPView.as_view(), name='get'),
+]
+
+
+@override_settings(ROOT_URLCONF=__name__)
+class OCSPTestView(DjangoCAWithCertTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(OCSPTestView, cls).setUpClass()
+        cls.client = Client()
+
+    def test_basic(self):
+        data = base64.b64encode(req1).decode('utf-8')
+        response = self.client.get(reverse('get', kwargs={'data': data}))
+        self.assertEqual(response.status_code, 200)
