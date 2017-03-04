269793f642e320304d0f1e27a8c2cb8e492105c7
Mathias Ertl <mati@er.tl>
2017-03-04 09:35:05 +0100

    add todo extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,6 +48,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
