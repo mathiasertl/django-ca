d4c71ff24fe26798566da35b381a0abb2139f284
Mathias Ertl <mati@er.tl>
2017-05-05 09:37:37 +0200

    start writing tests for import_ca

---
+++ ca/django_ca/tests/tests_command_import_ca.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of django-ca (https://github.com/mathiasertl/django-ca).
+#
+# django-ca is free software: you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# django-ca is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with django-ca.  If not,
+# see <http://www.gnu.org/licenses/>.
+
+import os
+from io import BufferedReader
+from datetime import timedelta
+
+from six import BytesIO
+
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import dsa
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
+
+from django.conf import settings
+from django.core.management.base import CommandError
+
+from django_ca.models import CertificateAuthority
+from django_ca.tests.base import DjangoCATestCase
+from django_ca.tests.base import override_tmpcadir
+
+from .. import ca_settings
+from ..utils import int_to_hex
+
+
+class ImportCATest(DjangoCATestCase):
+    def get_reader(self, path):
+        path = os.path.join(settings.FIXTURES_DIR, path)
+        with open(path, 'rb') as stream:
+            data = stream.read()
+
+        return BufferedReader(BytesIO(data))
+
+    def init_ca(self, **kwargs):
+        name = kwargs.pop('name', 'Test CA')
+        kwargs.setdefault('key_size', ca_settings.CA_MIN_KEY_SIZE)
+        return self.cmd('init_ca', name, '/C=AT/ST=Vienna/L=Vienna/O=Org/OU=OrgUnit/CN=%s' % name,
+                        **kwargs)
+
+    @override_tmpcadir(CA_MIN_KEY_SIZE=1024)
+    def test_basic(self):
+        pem_path = os.path.join(settings.FIXTURES_DIR, 'root.pem')
+        key_path = os.path.join(settings.FIXTURES_DIR, 'root.key')
+        out, err = self.cmd('import_ca', 'testname', key_path, pem_path)
+
+        self.assertEqual(out, '')
+        self.assertEqual(err, '')
+
+        ca = CertificateAuthority.objects.first()
+        self.assertSignature([ca], ca)
+        ca.full_clean()  # assert e.g. max_length in serials
+        self.assertBasic(ca.x509, algo='sha512')
+
+        # test the private key
+        key = ca.key(None)
+        self.assertIsInstance(key, RSAPrivateKey)
+        self.assertEqual(key.key_size, 1024)
+
+        self.assertSubject(ca.x509, {'C': 'AT', 'ST': 'Vienna', 'L': 'Vienna', 'O': 'Org',
+                                     'OU': 'OrgUnit', 'CN': 'Test CA'})
+        self.assertIssuer(ca, ca)
+        self.assertAuthorityKeyIdentifier(ca, ca)
+        self.assertEqual(ca.serial, int_to_hex(ca.x509.serial_number))
+
