837503be024e4d21805dfdd89bf311a36d5c814a
Mathias Ertl <mati@er.tl>
2017-05-05 13:06:56 +0200

    add more tests for importing certificates

--- ca/django_ca/tests/tests_command_import_cert.py
+++ ca/django_ca/tests/tests_command_import_cert.py
@@ -16,6 +16,7 @@
 import os
 
 from django.conf import settings
+from django.core.management.base import CommandError
 
 from django_ca.models import Certificate
 from django_ca.tests.base import DjangoCAWithCATestCase
@@ -37,3 +38,22 @@ class ImportCertTest(DjangoCAWithCATestCase):
         self.assertSignature([self.ca], cert)
         cert.full_clean()  # assert e.g. max_length in serials
         self.assertBasic(cert.x509, algo='sha512')
+
+    @override_tmpcadir(CA_MIN_KEY_SIZE=1024)
+    def test_der(self):
+        pem_path = os.path.join(settings.FIXTURES_DIR, 'cert1-pub.der')
+        out, err = self.cmd('import_cert', pem_path, ca=self.ca)
+
+        self.assertEqual(out, '')
+        self.assertEqual(err, '')
+
+        cert = Certificate.objects.get(serial=certs['cert1']['serial'])
+        self.assertSignature([self.ca], cert)
+        cert.full_clean()  # assert e.g. max_length in serials
+        self.assertBasic(cert.x509, algo='sha512')
+
+    @override_tmpcadir(CA_MIN_KEY_SIZE=1024)
+    def test_bogus(self):
+        with self.assertRaisesRegex(CommandError, '^Unable to load public key\.$'):
+            self.cmd('import_cert', __file__, ca=self.ca)
+        self.assertEqual(Certificate.objects.count(), 0)
