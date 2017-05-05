bbcc745035755a884f3d0af7be71eed4b9236612
Mathias Ertl <mati@er.tl>
2017-05-05 12:53:08 +0200

    start testing

---
+++ ca/django_ca/tests/tests_command_import_cert.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of django-ca (https://github.com/mathiasertl/django-ca).
+#
+# django-ca is free software: you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# django-ca is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with django-ca.  If not,
+# see <http://www.gnu.org/licenses/>.
+
+import os
+
+from django.conf import settings
+
+from django_ca.models import Certificate
+from django_ca.tests.base import DjangoCAWithCATestCase
+from django_ca.tests.base import override_tmpcadir
+
+from .base import certs
+
+
+class ImportCertTest(DjangoCAWithCATestCase):
+    @override_tmpcadir(CA_MIN_KEY_SIZE=1024)
+    def test_basic(self):
+        pem_path = os.path.join(settings.FIXTURES_DIR, 'cert1.pem')
+        out, err = self.cmd('import_cert', pem_path, ca=self.ca)
+
+        self.assertEqual(out, '')
+        self.assertEqual(err, '')
+
+        cert = Certificate.objects.get(serial=certs['cert1']['serial'])
+        self.assertSignature([self.ca], cert)
+        cert.full_clean()  # assert e.g. max_length in serials
+        self.assertBasic(cert.x509, algo='sha512')
