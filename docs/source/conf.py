8c316e0895c15a0faa64caac85ba540a0a2a38d4
Mathias Ertl <mati@er.tl>
2017-12-09 13:15:26 +0100

    enable numpydoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,6 +49,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.todo',
+    'numpydoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
