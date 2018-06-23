70de0b240cb4149af73b4e06814d2eb09ff92754
Mathias Ertl <mati@er.tl>
2018-06-23 13:02:42 +0200

    add paragraph on django apps

--- ca/django_ca/signals.py
+++ ca/django_ca/signals.py
@@ -17,6 +17,10 @@
 **django-ca** adds a few custom Django signals to important events to let you execute custom actions when
 these events happen. Please see `Djangos documentation on signals
 <https://docs.djangoproject.com/en/dev/ref/signals/>`_ for further information on how to use signals.
+
+If you use **django-ca** as :ref:`standalone project <as-standalone>`, use the :ref:`CA_CUSTOM_APPS
+<settings-ca-custom-apps>` setting to add a custom django app. Please see the `Django documentation on apps
+<https://docs.djangoproject.com/en/dev/ref/applications/>`_ if you need help on writing Django apps.
 """
 
 import django.dispatch
