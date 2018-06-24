95019ffc0f7f4247ab17bcaf5e7d6ae2ceb6f1de
Alex <amaccuish@users.noreply.github.com>
2018-06-24 02:22:19 +0000

    Update tests_utils.py

--- ca/django_ca/tests/tests_utils.py
+++ ca/django_ca/tests/tests_utils.py
@@ -265,7 +265,7 @@ class ParseGeneralNameTest(DjangoCATestCase):
 
     def test_otherName_octetString(self):
         self.assertEqual(parse_general_name('otherName:1.3.6.1.4.1.311.25.1;OctetString:09CFF1A8F6DEFD4B85CE95FFA1B54217'),
-                         x509.OtherName(ObjectIdentifier('1.3.6.1.4.1.311.25.1', 'Unknown OID'),
+                         x509.OtherName(x509.oid.ObjectIdentifier('1.3.6.1.4.1.311.25.1', 'Unknown OID'),
                                         value=b'\x04\x10\t\xcf\xf1\xa8\xf6\xde\xfdK\x85\xce\x95\xff\xa1\xb5B\x17'))
 
     def test_error(self):
