4c97968f222209d3212079efb252b64dfc5f5e60
Mathias Ertl <mati@er.tl>
2018-08-04 18:02:45 +0200

    remove unused import

--- ca/ca/test_settings.py
+++ ca/ca/test_settings.py
@@ -2,8 +2,6 @@
 
 import os
 
-from django import VERSION
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
