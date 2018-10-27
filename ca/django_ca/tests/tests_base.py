8205ae0400bba805c08f7fa8e7d4f21ef2a4631c
Mathias Ertl <mati@er.tl>
2018-10-27 16:21:18 +0200

    add some more test cases

--- ca/django_ca/tests/tests_base.py
+++ ca/django_ca/tests/tests_base.py
@@ -40,6 +40,15 @@ class OverrideSettingsFuncTestCase(TestCase):
         self.assertEqual(ca_settings.CA_MIN_KEY_SIZE, 256)
 
 
+@override_settings(CA_MIN_KEY_SIZE=512)
+class OverrideSettingsClassOnlyTestCase(DjangoCATestCase):
+    def test_basic(self):
+        self.assertEqual(ca_settings.CA_MIN_KEY_SIZE, 512)
+
+    def test_second(self):
+        self.assertEqual(ca_settings.CA_MIN_KEY_SIZE, 512)
+
+
 @override_settings(CA_MIN_KEY_SIZE=128)
 class OverrideSettingsClassTestCase(DjangoCATestCase):
     def test_basic(self):
@@ -54,3 +63,29 @@ class OverrideSettingsClassTestCase(DjangoCATestCase):
             @override_settings(CA_MIN_KEY_SIZE=256)
             class DummyTest(TestCase):
                 pass
+
+
+class OverrideCaDirForFuncTestCase(DjangoCATestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(OverrideCaDirForFuncTestCase, cls).setUpClass()
+        cls.seen_dirs = set()
+
+    @override_tmpcadir()
+    def test_a(self):
+        # add three tests to make sure that every test case sees a different dir
+        self.assertTrue(ca_settings.CA_DIR.startswith(tempfile.gettempdir()), ca_settings.CA_DIR)
+        self.assertNotIn(ca_settings.CA_DIR, self.seen_dirs)
+        self.seen_dirs.add(ca_settings.CA_DIR)
+
+    @override_tmpcadir()
+    def test_b(self):
+        self.assertTrue(ca_settings.CA_DIR.startswith(tempfile.gettempdir()), ca_settings.CA_DIR)
+        self.assertNotIn(ca_settings.CA_DIR, self.seen_dirs)
+        self.seen_dirs.add(ca_settings.CA_DIR)
+
+    @override_tmpcadir()
+    def test_c(self):
+        self.assertTrue(ca_settings.CA_DIR.startswith(tempfile.gettempdir()), ca_settings.CA_DIR)
+        self.assertNotIn(ca_settings.CA_DIR, self.seen_dirs)
+        self.seen_dirs.add(ca_settings.CA_DIR)
