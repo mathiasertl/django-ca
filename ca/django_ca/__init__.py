b68593346f2ba4b2381d329353842e191cc024e4
Mathias Ertl <mati@er.tl>
2019-01-26 13:49:00 +0100

    add some links for future reference

--- ca/django_ca/__init__.py
+++ ca/django_ca/__init__.py
@@ -1,4 +1,10 @@
-# https://www.python.org/dev/peps/pep-0440/#local-version-identifiers
+# WARNING: This module MUST NOT include any dependencys, as it is read by setup.py
+
+# https://www.python.org/dev/peps/pep-0440/
+# https://www.python.org/dev/peps/pep-0396/
+# https://www.python.org/dev/peps/pep-0386/
 VERSION = (1, 12, 0, 'dev', 1)
+
+# __version__ specified in PEP 0396, but we use PEP 0440 format instead of PEP 0386.
 __version__ = '1.12.dev1'
 default_app_config = 'django_ca.apps.DjangoCAConfig'
