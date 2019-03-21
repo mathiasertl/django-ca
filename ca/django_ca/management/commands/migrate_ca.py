b57665a434461cefe1d136a15d0a658bcb3621e7
Mathias Ertl <mati@er.tl>
2019-03-21 09:13:01 +0100

    code quality

--- ca/django_ca/management/commands/migrate_ca.py
+++ ca/django_ca/management/commands/migrate_ca.py
@@ -13,8 +13,8 @@
 # You should have received a copy of the GNU General Public License along with django-ca.  If not,
 # see <http://www.gnu.org/licenses/>.
 
-import shutil
 import os
+import shutil
 
 from django.core.files.storage import FileSystemStorage
 from django.core.management.base import CommandError
