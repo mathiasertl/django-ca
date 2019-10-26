4b9b74d4a24629ad5848bcf3b2503fc0cd7f0351
Mathias Ertl <mati@er.tl>
2019-10-26 13:10:15 +0200

    fix js

--- ca/django_ca/static/django_ca/admin/js/profilewidget.js
+++ ca/django_ca/static/django_ca/admin/js/profilewidget.js
@@ -49,20 +49,20 @@ django.jQuery(document).ready(function() {
         extensions.map(function(ext) {
             var critical_selector = '.field-' + ext + ' .critical-widget-wrapper input';
             var value_selector = '.field-' + ext + ' select';
-            if (profile[ext] == null) {
+            if (profile.extensions[ext] == null) {
                 // the extension may be null, meaning the extension should not be added
                 django.jQuery(critical_selector).prop('checked', false);
                 django.jQuery(value_selector).val([]);
                 django.jQuery(value_selector).change();  // so any existing callbacks are called
             } else {
-                django.jQuery(critical_selector).prop('checked', profile[ext].critical);
-                django.jQuery(value_selector).val(profile[ext].value);
+                django.jQuery(critical_selector).prop('checked', profile.extensions[ext].critical);
+                django.jQuery(value_selector).val(profile.extensions[ext].value);
                 django.jQuery(value_selector).change();  // so any existing callbacks are called
             }
         });
 
         // update description
         django.jQuery('.field-profile .profile-desc').show();
-        django.jQuery('.field-profile .profile-desc').text(profile.desc);
+        django.jQuery('.field-profile .profile-desc').text(profile.description);
     });
 });
