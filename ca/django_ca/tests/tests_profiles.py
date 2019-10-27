628b229f6c0dd3ab89dabecfd465c0958fd0098d
Mathias Ertl <mati@er.tl>
2019-10-27 11:12:18 +0100

    test more profile functions

--- ca/django_ca/tests/tests_profiles.py
+++ ca/django_ca/tests/tests_profiles.py
@@ -16,11 +16,78 @@
 from .. import ca_settings
 from ..extensions import ExtendedKeyUsage
 from ..extensions import KeyUsage
+from ..extensions import OCSPNoCheck
 from ..extensions import TLSFeature
+from ..profiles import Profile
 from ..profiles import get_cert_profile_kwargs
 from ..profiles import get_profile
+from ..profiles import profiles
+from ..profiles import profile
+from ..subject import Subject
 from .base import DjangoCATestCase
 from .base import override_settings
+
+
+class ProfileTestCase(DjangoCATestCase):
+    def test_eq(self):
+        p = None
+        for name in ca_settings.CA_PROFILES:
+            self.assertNotEqual(p, profiles[name])
+            p = profiles[name]
+            self.assertEqual(p, p)
+
+    def test_init_django_ca_values(self):
+        p1 = Profile('test', subject=Subject('/C=AT/CN=example.com'), extensions={
+            OCSPNoCheck.key: {},
+        })
+        p2 = Profile('test', subject='/C=AT/CN=example.com', extensions={
+            OCSPNoCheck.key: OCSPNoCheck({}),
+        })
+        self.assertEqual(p1, p2)
+
+    def test_init_ld_values(self):
+        name = 'example'
+        subject = Subject('/C=AT/L=Vienna')
+        desc = 'example description'
+        ku = {'value': ['keyAgreement', 'keyEncipherment']}
+        eku = {'value': ['clientAuth', 'serverAuth']}
+        tf = {'value': ['OCSPMustStaple']}
+
+        p1 = Profile(name, subject=subject, description=desc, extensions={
+            KeyUsage.key: ku,
+            ExtendedKeyUsage.key: eku,
+            TLSFeature.key: tf,
+            OCSPNoCheck.key: {},
+        })
+        with self.assertMultipleWarnings([
+            {'category': DeprecationWarning, 'filename': __file__,
+             'msg': r'^keyUsage in profile is deprecated, use extensions -> key_usage instead\.$',
+            },
+            {'category': DeprecationWarning, 'filename': __file__,
+             'msg': r'^extendedKeyUsage in profile is deprecated, use extensions -> extended_key_usage instead\.$',   # NOQA
+            },
+            {'category': DeprecationWarning, 'filename': __file__,
+             'msg': r'^TLSFeature in profile is deprecated, use extensions -> tls_feature instead\.$',
+            },
+            {'category': DeprecationWarning, 'filename': __file__,
+             'msg': r'^desc in profile is deprecated, use description instead\.$',
+            },
+            {'category': DeprecationWarning, 'filename': __file__,
+             'msg': r'^ocsp_no_check in profile is deprecated, use extensions -> ocsp_no_check instead\.$',
+            },
+        ]):
+            p2 = Profile(name, subject=subject, desc=desc, keyUsage=ku, extendedKeyUsage=eku, TLSFeature=tf,
+                         ocsp_no_check=True)
+
+        self.assertEqual(p1, p2)
+
+    def test_str(self):
+        for name in ca_settings.CA_PROFILES:
+            self.assertEqual(str(profiles[name]), "<Profile: '%s'>" % name)
+
+    def test_repr(self):
+        for name in ca_settings.CA_PROFILES:
+            self.assertEqual(repr(profiles[name]), "<Profile: '%s'>" % name)
 
 
 class GetProfileTestCase(DjangoCATestCase):
@@ -31,6 +98,26 @@ class GetProfileTestCase(DjangoCATestCase):
 
         profile = get_profile()
         self.assertEqual(profile.name, ca_settings.CA_DEFAULT_PROFILE)
+
+
+class ProfilesTestCase(DjangoCATestCase):
+    def test_basic(self):
+        for name in ca_settings.CA_PROFILES:
+            p = profiles[name]
+            self.assertEqual(p.name, name)
+
+        # Run a second time, b/c accessor also caches stuff sometimes
+        for name in ca_settings.CA_PROFILES:
+            p = profiles[name]
+            self.assertEqual(p.name, name)
+
+    def test_default_proxy(self):
+        self.assertEqual(profile.name, ca_settings.CA_DEFAULT_PROFILE)
+        self.assertEqual(str(profile), "<DefaultProfile: '%s'>" % ca_settings.CA_DEFAULT_PROFILE)
+        self.assertEqual(repr(profile), "<DefaultProfile: '%s'>" % ca_settings.CA_DEFAULT_PROFILE)
+
+        self.assertEqual(profile, profile)
+        self.assertEqual(profile, profiles[ca_settings.CA_DEFAULT_PROFILE])
 
 
 class GetCertProfileKwargsTestCase(DjangoCATestCase):
@@ -67,7 +154,6 @@ class GetCertProfileKwargsTestCase(DjangoCATestCase):
         },
     })
     def test_ocsp_no_check(self):
-        self.maxDiff = None
         expected = {
             'cn_in_san': True,
             'ocsp_no_check': True,
