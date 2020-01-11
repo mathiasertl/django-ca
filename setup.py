88b87d5b2a06661701c6a35b412aebc5808596ff
Mathias Ertl <mathias.ertl@tttech.com>
2020-01-11 15:22:11 +0100

    update version parameter

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ package_data = find_package_data('static') + find_package_data('templates')
 
 setup(
     name='django-ca',
-    version='1.15.0.dev1',
+    version='1.15.0',
     description='A Django app providing a SSL/TLS certificate authority.',
     long_description=long_description,
     author='Mathias Ertl',
