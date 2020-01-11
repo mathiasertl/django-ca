7758b7ae3b33efeb8c2a6d782809a05eb40eae71
Mathias Ertl <mati@er.tl>
2020-01-11 18:11:34 +0100

    identify as 1.15.0

--- ca/django_ca/__init__.py
+++ ca/django_ca/__init__.py
@@ -3,8 +3,8 @@
 # https://www.python.org/dev/peps/pep-0440/
 # https://www.python.org/dev/peps/pep-0396/
 # https://www.python.org/dev/peps/pep-0386/
-VERSION = (1, 15, 0, 'dev', 1)
+VERSION = (1, 15, 0)
 
 # __version__ specified in PEP 0396, but we use PEP 0440 format instead of PEP 0386.
-__version__ = '1.15.0.dev1'
+__version__ = '1.15.0'
 default_app_config = 'django_ca.apps.DjangoCAConfig'
