a16c9c867f9127cd8f3c62beda5e6986de5ecc63
Mathias Ertl <mati@er.tl>
2020-08-07 18:12:40 +0200

    prepare 1.16.0 release

--- dev.py
+++ dev.py
@@ -270,6 +270,9 @@ elif args.command == 'docker-test':
         # Currently supported Alpine releases:
         #   https://wiki.alpinelinux.org/wiki/Alpine_Linux:Releases
 
+        'python:3.6-alpine3.12',
+        'python:3.7-alpine3.12',
+        'python:3.8-alpine3.12',
         'python:3.5-alpine3.11',
         'python:3.6-alpine3.11',
         'python:3.7-alpine3.11',
@@ -278,10 +281,6 @@ elif args.command == 'docker-test':
         'python:3.6-alpine3.10',
         'python:3.7-alpine3.10',
         'python:3.8-alpine3.10',
-        'python:3.9-rc-alpine3.10',
-        'python:3.5-alpine3.9',
-        'python:3.6-alpine3.9',
-        'python:3.7-alpine3.9',
     ]
 
     docker_runs = []
