2253fe535921be0854cde1de125ebda14d2a4ed3
Mathias Ertl <mati@er.tl>
2020-10-18 12:55:39 +0200

    test new CA_DEFAULT_CA setting, pylint cleanup

--- ca/django_ca/tests/tests_settings.py
+++ ca/django_ca/tests/tests_settings.py
@@ -11,6 +11,8 @@
 # You should have received a copy of the GNU General Public License along with django-ca.  If not,
 # see <http://www.gnu.org/licenses/>
 
+"""Test cases for the ``ca_settings`` module."""
+
 from datetime import timedelta
 from unittest import mock
 
@@ -22,27 +24,65 @@ from .base import DjangoCATestCase
 
 
 class SettingsTestCase(DjangoCATestCase):
+    """Test some standard settings."""
+
     def test_none_profiles(self):
+        """Test removing a profile by setting it to None."""
         self.assertIn('client', ca_settings.CA_PROFILES)
 
         with self.settings(CA_PROFILES={'client': None}):
             self.assertNotIn('client', ca_settings.CA_PROFILES)
 
     def test_ca_profile_update(self):
+        """Test adding a profile in settings."""
         desc = 'testdesc'
         with self.settings(CA_PROFILES={'client': {'desc': desc}}):
             self.assertEqual(ca_settings.CA_PROFILES['client']['desc'], desc)
 
-    def test_missing_celery(self):
+    def test_use_celery(self):
+        """Test CA_USE_CELERY setting."""
+        with self.settings(CA_USE_CELERY=False):
+            self.assertFalse(ca_settings.CA_USE_CELERY)
+        with self.settings(CA_USE_CELERY=True):
+            self.assertTrue(ca_settings.CA_USE_CELERY)
+        with self.settings(CA_USE_CELERY=None):
+            self.assertTrue(ca_settings.CA_USE_CELERY)
+
+        # mock a missing Celery installation
         with mock.patch.dict('sys.modules', celery=None), self.settings(CA_USE_CELERY=None):
+            self.assertFalse(ca_settings.CA_USE_CELERY)
+        with mock.patch.dict('sys.modules', celery=None), self.settings(CA_USE_CELERY=False):
             self.assertFalse(ca_settings.CA_USE_CELERY)
 
 
+class DefaultCATestCase(DjangoCATestCase):
+    """Test the :ref:`CA_DEFAULT_CA <settings-ca-default-ca>` setting."""
+
+    def test_no_setting(self):
+        """Test empty setting."""
+        with self.settings(CA_DEFAULT_CA=''):
+            self.assertEqual(ca_settings.CA_DEFAULT_CA, '')
+
+    def test_unsanitized_setting(self):
+        """Test that values are sanitized properly."""
+        with self.settings(CA_DEFAULT_CA='0a:bc'):
+            self.assertEqual(ca_settings.CA_DEFAULT_CA, 'ABC')
+
+    def test_serial_zero(self):
+        """Test that a '0' serial is not stripped."""
+        with self.settings(CA_DEFAULT_CA='0'):
+            self.assertEqual(ca_settings.CA_DEFAULT_CA, '0')
+
+
 class ImproperlyConfiguredTestCase(DjangoCATestCase):
-    def assertImproperlyConfigured(self, msg):
+    """Test various invalid configurations."""
+
+    def assertImproperlyConfigured(self, msg):  # pylint: disable=invalid-name; unittest standard
+        """Minor shortcut to ``assertRaisesRegex``."""
         return self.assertRaisesRegex(ImproperlyConfigured, msg)
 
     def test_default_ecc_curve(self):
+        """Test invalid ``CA_DEFAULT_ECC_CURVE``."""
         with self.assertImproperlyConfigured(r'^Unkown CA_DEFAULT_ECC_CURVE: foo$'):
             with self.settings(CA_DEFAULT_ECC_CURVE='foo'):
                 pass
@@ -56,11 +96,13 @@ class ImproperlyConfiguredTestCase(DjangoCATestCase):
                 pass
 
     def test_digest_algorithm(self):
+        """Test invalid ``CA_DIGEST_ALGORITHM``."""
         with self.assertImproperlyConfigured(r'^Unkown CA_DIGEST_ALGORITHM: foo$'):
             with self.settings(CA_DIGEST_ALGORITHM='foo'):
                 pass
 
     def test_default_expires(self):
+        """Test invalid ``CA_DEFAULT_EXPIRES``."""
         with self.assertImproperlyConfigured(r'^CA_DEFAULT_EXPIRES: foo: Must be int or timedelta$'):
             with self.settings(CA_DEFAULT_EXPIRES='foo'):
                 pass
@@ -71,6 +113,7 @@ class ImproperlyConfiguredTestCase(DjangoCATestCase):
                 pass
 
     def test_default_subject(self):
+        """Test invalid ``CA_DEFAULT_SUBJECT``."""
         with self.assertImproperlyConfigured(r'^CA_DEFAULT_SUBJECT: Invalid subject: True$'):
             with self.settings(CA_DEFAULT_SUBJECT=True):
                 get_default_subject()
@@ -80,14 +123,7 @@ class ImproperlyConfiguredTestCase(DjangoCATestCase):
                 get_default_subject()
 
     def test_use_celery(self):
-        # test CA_USE_CELERY setting
-        with self.settings(CA_USE_CELERY=False):
-            self.assertFalse(ca_settings.CA_USE_CELERY)
-        with self.settings(CA_USE_CELERY=True):
-            self.assertTrue(ca_settings.CA_USE_CELERY)
-        with self.settings(CA_USE_CELERY=None):
-            self.assertTrue(ca_settings.CA_USE_CELERY)
-
+        """Test that CA_USE_CELERY=True and a missing Celery installation throws an error."""
         # Setting sys.modules['celery'] (modules cache) to None will cause the next import of that module
         # to trigger an import error:
         #   https://medium.com/python-pandemonium/how-to-test-your-imports-1461c1113be1
@@ -96,4 +132,10 @@ class ImproperlyConfiguredTestCase(DjangoCATestCase):
             msg = r'^CA_USE_CELERY set to True, but Celery is not installed$'
 
             with self.assertImproperlyConfigured(msg), self.settings(CA_USE_CELERY=True):
+                pass
+
+    def test_invalid_setting(self):
+        """Test setting an invalid CA."""
+        with self.assertImproperlyConfigured(r'^CA_DEFAULT_CA: ABCX: Serial contains invalid characters\.$'):
+            with self.settings(CA_DEFAULT_CA='0a:bc:x'):
                 pass
