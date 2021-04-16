6230e2456f2c620b97e88230564eb3534f69cc4d
Mathias Ertl <mati@er.tl>
2021-04-17 01:06:57 +0200

    fix cover

--- ca/django_ca/tests/base.py
+++ ca/django_ca/tests/base.py
@@ -656,7 +656,7 @@ VQIDAQAB
                     OID_TO_EXTENSION[e.oid](e) if e.oid in OID_TO_EXTENSION else e for e in cert.extensions
                 ]
             }
-        else:  # pragma: no branch
+        else:  # pragma: no cover
             raise ValueError("cert must be Certificate(Authority) or x509.Certificate)")
 
         if expect_defaults is True:
