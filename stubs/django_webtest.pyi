2ac8877b39ac83c242c88861d6834ae761f83dac
Mathias Ertl <mati@er.tl>
2021-04-19 17:31:48 +0200

    start webtest stub

---
+++ stubs/django_webtest.pyi
@@ -0,0 +1,27 @@
+import typing
+
+from django.http import HttpResponse
+
+
+class Form:
+    def submit(self) -> "DjangoWebtestResponse":
+        ...
+
+
+class DjangoWebtestResponse:
+    def follow(self) -> "DjangoWebtestResponse":
+        ...
+
+    @property
+    def form(self) -> Form:
+        ...
+
+
+class DjangoTestApp:
+    def get(self, url: str, *args: typing.Any, **kwargs: typing.Any) -> DjangoWebtestResponse:
+        ...
+
+
+class WebTestMixin:
+    app_class: DjangoTestApp = typing.Type[DjangoTestApp]
+    app: DjangoTestApp
