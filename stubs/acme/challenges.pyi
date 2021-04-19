2ed61b83d3e4ad11bcbaf1d9e18f77a22300eb96
Mathias Ertl <mati@er.tl>
2021-04-19 13:50:07 +0200

    fix filename

---
+++ stubs/acme/challenges.pyi
@@ -0,0 +1,50 @@
+# pylint: skip-file
+from typing import Type
+
+import josepy as jose
+from cryptography.hazmat.primitives import hashes
+
+from acme.mixins import ResourceMixin
+from acme.mixins import TypeMixin
+
+
+class Challenge(jose.TypedJSONObjectWithFields):
+    ...
+
+
+class ChallengeResponse(ResourceMixin, TypeMixin, jose.TypedJSONObjectWithFields):
+    ...
+
+
+class _TokenChallenge(Challenge):
+    TOKEN_SIZE: int
+    token = str
+
+
+class KeyAuthorizationChallengeResponse(ChallengeResponse):
+    thumbprint_hash_function = Type[hashes.HashAlgorithm]
+
+
+class KeyAuthorizationChallenge(_TokenChallenge):
+    def __init__(self, token: bytes):
+        ...
+
+
+class DNS01(KeyAuthorizationChallenge):
+    ...
+
+
+class HTTP01(KeyAuthorizationChallenge):
+    ...
+
+
+class HTTP01Response(KeyAuthorizationChallengeResponse):
+    ...
+
+
+class TLSALPN01(KeyAuthorizationChallenge):
+    ...
+
+
+class TLSALPN01Response(KeyAuthorizationChallengeResponse):
+    ...
