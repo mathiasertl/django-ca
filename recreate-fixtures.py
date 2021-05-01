10e46d951819fdf1666260665f6a9285728c80c7
Mathias Ertl <mati@er.tl>
2021-05-01 16:15:39 +0200

    force settings module again

--- recreate-fixtures.py
+++ recreate-fixtures.py
@@ -50,7 +50,7 @@ except ImportError:
 _rootdir = os.path.dirname(os.path.realpath(__file__))  # NOQA: E402
 _sphinx_dir = os.path.join(_rootdir, "docs", "source", "_files")  # NOQA: E402
 sys.path.insert(0, os.path.join(_rootdir, "ca"))  # NOQA: E402
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ca.test_settings")  # NOQA: E402
+os.environ["DJANGO_SETTINGS_MODULE"] = "ca.test_settings"  # NOQA: E402
 # pylint: disable=wrong-import-position
 import django  # NOQA: E402 isort:skip
 
