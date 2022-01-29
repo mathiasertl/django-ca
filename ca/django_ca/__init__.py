68baeeb3f5d9c1f8eaddf03eb25ba5055557041c
Mathias Ertl <mati@er.tl>
2022-01-29 15:25:20 +0100

    fix/update version and coverage

--- ca/django_ca/__init__.py
+++ ca/django_ca/__init__.py
@@ -1,12 +1,13 @@
+"""django-ca root module."""
+
 # WARNING: This module MUST NOT include any dependencies, as it is read by setup.py
 
 # https://www.python.org/dev/peps/pep-0440/
 # https://www.python.org/dev/peps/pep-0396/
 # https://www.python.org/dev/peps/pep-0386/
-VERSION = (1, 20, 0, "dev", 1)
-VERSION = (1, 20, 0)
+VERSION = (1, 21, 0, "dev", 1)
 
 # __version__ specified in PEP 0396, but we use the PEP 0440 format instead
 __version__ = ".".join([str(e) for e in VERSION[:3]])
-if len(VERSION) > 3:
+if len(VERSION) > 3:  # pragma: no cover
     __version__ += f".{''.join(str(e) for e in  VERSION[3:])}"
