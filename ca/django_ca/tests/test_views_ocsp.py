bf2e6a12e560f194eeb480095c7fad3bb3d454c6
Mathias Ertl <mati@er.tl>
2024-03-02 12:32:44 +0100

    fix more test cases

--- ca/django_ca/tests/test_views_ocsp.py
+++ ca/django_ca/tests/test_views_ocsp.py
@@ -38,6 +38,7 @@ from django.urls import path, re_path, reverse
 from freezegun import freeze_time
 
 from django_ca import ca_settings
+from django_ca.backends.storages import LoadPrivateKeyOptions
 from django_ca.constants import ReasonFlags
 from django_ca.modelfields import LazyCertificate
 from django_ca.models import Certificate, CertificateAuthority
@@ -302,7 +303,8 @@ class OCSPViewTestMixin(TestCaseMixin):
         self, ca: CertificateAuthority
     ) -> Tuple[CertificateIssuerPrivateKeyTypes, Certificate]:
         """Generate an OCSP key for the given CA and return private kay and public key model instance."""
-        priv_path, _cert_path, ocsp_cert = ca.generate_ocsp_key()  # type: ignore[misc]
+        key_backend_options = LoadPrivateKeyOptions(password=CERT_DATA[ca.name].get("password"))
+        priv_path, _cert_path, ocsp_cert = ca.generate_ocsp_key(key_backend_options)  # type: ignore[misc]
         with storages["django-ca"].open(priv_path, "rb") as stream:
             private_key = typing.cast(
                 CertificateIssuerPrivateKeyTypes, load_der_private_key(stream.read(), None)
